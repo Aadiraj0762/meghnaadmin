3ecec793eac727bbd088e9a1bfcdb14bddd8c461
Aadiraj0762 <narayaneaadiraj@gmail.com>
2024-05-25 23:08:16 +0530

    backend upload user template modified build upload

--- src/layout/FullTestimonial.js
+++ src/layout/FullTestimonial.js
@@ -2,7 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import img1 from "../../public/user.png";
+// import img1 from "../../public/user.png";
 const testimonials = [
   {
     id: 1,
